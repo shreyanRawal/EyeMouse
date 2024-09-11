81405e6a619125bd47ab83333f705f759d881b3b
shreyanRawal <119391878+shreyanRawal@users.noreply.github.com>
2024-09-11 18:35:53 +0545

    few changes, eye indicator

--- eyeMouse.py
+++ eyeMouse.py
@@ -6,17 +6,25 @@ face_landmark = mediapipe.solutions.face_mesh.FaceMesh(refine_landmarks = True) 
 camera = cv2.VideoCapture(0) #have only 1 camera
 while True:
      _, image = camera.read()
+     image = cv2.flip(image,1) #lipping image as it shows opposite, 1 for horizontal
      window_height, window_width, _ = image.shape
      rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) #converting to rgb format from bgr format
      processed_image = face_landmark.process(rgb_image) #processing the changed image 
      all_faces = processed_image.multi_face_landmarks #storing all landmarks of faces
      if all_faces:
           one_face = all_faces[0].landmark #getting landmark of one face, ie the first detected face
-          for landmark_point in one_face:
+          for landmark_point in one_face[474:478]: #points number of eyes
                x = int(landmark_point.x * window_width) #changing the decimals according to window width and height 
                y = int(landmark_point.y * window_height) #using int
                print(x, y) 
                cv2.circle(image,(x,y),3,(0,0,225))
+          left_eye = [one_face[145],one_face[159]]  #getting points of left eye
+          for landmark_point in left_eye: #same for let eye
+               x = int(landmark_point.x * window_width) 
+               y = int(landmark_point.y * window_height) 
+               print(x, y) 
+               cv2.circle(image,(x,y),3,(0,0,10))     
+        
      cv2.imshow("Eye mouse" , image)
      key = cv2.waitKey(100)
      if key ==27: #escape key code
