947fce3cdc04b3940f2971b33a4369806128a6af
shreyanRawal <119391878+shreyanRawal@users.noreply.github.com>
2024-09-11 16:54:33 +0545

    used mediapipe to see the image landmarks and also reined it, the used image was in bgr ormat, converted to rgb format

--- eyeMouse.py
+++ eyeMouse.py
@@ -1,11 +1,15 @@
 import cv2
 import mediapipe
 import pyautogui
-
+landmark = mediapipe.solutions.face_mesh.FaceMesh(refine_landmarks = True) #using mediapipe solutions and faceMesh to detect facial landmarks. reining it to make it more accurate in some areas
 
 camera = cv2.VideoCapture(0) #have only 1 camera
 while True:
      _, image = camera.read()
+     rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) #converting to rgb format from bgr format
+     processed_image = landmark.process(rgb_image) #processing the changed image 
+     all_faces = processed_image.multi_face_landmarks #storing all landmarks of faces
+     print(all_faces)
      cv2.imshow("Eye mouse" , image)
      key = cv2.waitKey(100)
      if key ==27: #escape key code
