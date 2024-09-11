d35d451e45a76dc07970cf0b0065bc7cead7040c
shreyanRawal <119391878+shreyanRawal@users.noreply.github.com>
2024-09-11 18:03:51 +0545

    Added real-time facial landmark detection with accurate circle overlays using MediaPipe FaceMesh

--- eyeMouse.py
+++ eyeMouse.py
@@ -6,17 +6,17 @@ face_landmark = mediapipe.solutions.face_mesh.FaceMesh(refine_landmarks = True) 
 camera = cv2.VideoCapture(0) #have only 1 camera
 while True:
      _, image = camera.read()
-     window_width , window_height, _ = image.shape
+     window_height, window_width, _ = image.shape
      rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) #converting to rgb format from bgr format
      processed_image = face_landmark.process(rgb_image) #processing the changed image 
      all_faces = processed_image.multi_face_landmarks #storing all landmarks of faces
-     print(all_faces)
      if all_faces:
-          one_face = all_faces[0].landmark
+          one_face = all_faces[0].landmark #getting landmark of one face, ie the first detected face
           for landmark_point in one_face:
-               print(landmark_point.x , landmark_point.y , landmark_point.z) 
-               x = landmark_point.x * window_width #changing the decimals according to window width and height 
-               y = landmark_point.y * window_height
+               x = int(landmark_point.x * window_width) #changing the decimals according to window width and height 
+               y = int(landmark_point.y * window_height) #using int
+               print(x, y) 
+               cv2.circle(image,(x,y),3,(0,0,225))
      cv2.imshow("Eye mouse" , image)
      key = cv2.waitKey(100)
      if key ==27: #escape key code
