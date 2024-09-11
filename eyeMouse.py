89dc7ce13a991713748f384a969914678f34af65
shreyanRawal <119391878+shreyanRawal@users.noreply.github.com>
2024-09-11 16:35:11 +0545

    cv2 used to get video from deault camera and show it in window, then used esc key to wait or exit in 100 milliseconds

--- eyeMouse.py
+++ eyeMouse.py
@@ -1,1 +1,14 @@
-print("Hello")
+import cv2
+import mediapipe
+import pyautogui
+
+
+camera = cv2.VideoCapture(0) #have only 1 camera
+while True:
+     _, image = camera.read()
+     cv2.imshow("Eye mouse" , image)
+     key = cv2.waitKey(100)
+     if key ==27: #escape key code
+          break 
+camera.release()
+cv2.destroyAllWindows()
